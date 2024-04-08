822f606edf916491ac646894e05d94b38e6a911e
Chris Cooley <ccooley@bonadmin.com>
2024-04-08 08:56:11 -0600

    Feat: add functionality to get, post, put, delete

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import uuid
 
 from fastapi import FastAPI
 
+from models.player import Player, CreatePlayerRequest, PlayerResponse
 from models.course import Course
-from models.player import Player
 
 
 app = FastAPI()
@@ -11,25 +11,27 @@ app = FastAPI()
 players: dict[uuid.UUID, Player] = {}
 courses: dict[uuid.UUID, Course] = {}
 
-@app.get("/players")
+@app.get("/player")
+async def get_players() -> list[Player]:
+    return players.values()
 
+@app.post("/player")
+async def create_player(player_detail: CreatePlayerRequest) -> uuid.UUID:
+    player_id = uuid.uuid4()
+    player = Player(id=player_id, name=player_detail.name, handicap=player_detail.handicap)
+    players[player.id] = player
+    return player.id
 
-# @app.post("/players")
+@app.put("/player/{player_id}")
+async def update_player(player_id: uuid.UUID, new_player: CreatePlayerRequest):
+    updated_player = Player(id=player_id, name=new_player.name, handicap=new_player.handicap)
+    players[player_id] = updated_player
 
-
-# @app.put("/players/{player_id}")
-
-
-# @app.delete("/players/{player_id}")
-
+@app.delete("/player/{player_id}")
+async def delete_player(player_id: uuid.UUID):
+    del players[player_id]
 
 # @app.get("/courses")
-
-
 # @app.post("/courses")
-
-
 # @app.put("/courses/{course_id}")
-
-
 # @app.delete("/courses/{course_id}")
